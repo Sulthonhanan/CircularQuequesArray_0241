52a763f29be086b15691a13752b2f7944d6b33a4
Sulthonhanan <hananakhnaf31@gmail.com>
2024-05-30 14:40:06 +0700

    mengecek apakah ada elemen di dalam indeks

--- CircularQuequesArray_0241/CircularQuequesArray_0241.cpp
+++ CircularQuequesArray_0241/CircularQuequesArray_0241.cpp
@@ -29,7 +29,41 @@ public:
 			return;
 		}
 
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array, kembai ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue undeflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
 
 
 
